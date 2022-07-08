56628d2c6c5917a65d6f00f8aff0ab80bd444a18
Edgarvalcarcel <edgarvalcarcel@hotmail.com>
2022-07-07 20:23:39 -0600

    FIX EMAIL

--- files/assets/inc/sendemail.php
+++ files/assets/inc/sendemail.php
@@ -1,8 +1,8 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "John Doe" );
-define( "RECIPIENT_EMAIL", "mail@mail.com" );
+define( "RECIPIENT_NAME", "Need help" );
+define( "RECIPIENT_EMAIL", "needhelp@charcolsoftware.com" );
 
 // Read the form values
 $success = false;
